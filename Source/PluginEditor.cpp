00cf29975b780ca5711c7c9731a780fc67dd36f3
Javaughn S <savaughnjmith@gmail.com>
2024-06-14 17:23:58 -0400

    add sliders to non-generic gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,13 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    for ( auto* comp : getComps() )
+    {
+        addAndMakeVisible(comp);
+    }
+    
+    setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
@@ -37,4 +43,34 @@ void SimpleEQAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33); // the top 33% of the whole window vertically
+    
+    auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33); // the left 33% of the bottom 67% of the window
+    auto highCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5); // the right half of the remaining horizontal 67% (the right 33.5%)
+    
+    lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(bounds.getHeight() * 0.5));
+    lowCutSlopeSlider.setBounds(lowCutArea);
+    
+    highCutFreqSlider.setBounds(highCutArea.removeFromTop(bounds.getHeight() * 0.5));
+    highCutSlopeSlider.setBounds(highCutArea);
+    
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33)); // the top 3rd of the remaining centre column
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5)); // the middle 3rd of the centre column
+    peakQualitySlider.setBounds(bounds); // the remaining bottom middle 3rd of the window
+}
+
+std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
+{
+    return
+    {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &peakQualitySlider,
+        &lowCutFreqSlider,
+        &highCutFreqSlider,
+        &lowCutSlopeSlider,
+        &highCutSlopeSlider
+    };
 }
