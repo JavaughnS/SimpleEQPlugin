3fb5769af5ce7f97fd0423ad10cc61236de6c227
Javaughn S <savaughnjmith@gmail.com>
2024-06-28 11:16:02 -0400

    gave editor its own monochain

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -27,6 +27,24 @@ struct ChainSettings
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
 
+//For simplicity's sake, setting aliases (like nicknames)
+// for classes within nested namespaces that we'll be using
+
+// Creating an alias for the Filter<float> class in the juce::dsp::IIR namespace
+// simplifying it to just Filter
+using Filter = juce::dsp::IIR::Filter<float>;
+
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
 //==============================================================================
 /**
 */
@@ -75,25 +93,7 @@ public:
     
 
 private:
-    //For simplicity's sake, setting aliases (like nicknames)
-    // for classes within nested namespaces that we'll be using
-    
-    // Creating an alias for the Filter<float> class in the juce::dsp::IIR namespace
-    // simplifying it to just Filter
-    using Filter = juce::dsp::IIR::Filter<float>;
-    
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
-    
     MonoChain leftChain, rightChain;
-    
-    enum ChainPositions
-    {
-        LowCut,
-        Peak,
-        HighCut
-    };
     
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
